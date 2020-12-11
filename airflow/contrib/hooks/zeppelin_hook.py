27e25f1802817be029b8a5fba68ac0eaf27301a3
jeffzhang.zjf <jeffzhang.zjf@alibaba-inc.com>
2020-12-11 17:18:21 +0800

    update zeppelin hook

--- airflow/contrib/hooks/zeppelin_hook.py
+++ airflow/contrib/hooks/zeppelin_hook.py
@@ -1,5 +1,5 @@
 from airflow.hooks.base_hook import BaseHook
-from pyzeppelin import ClientConfig, ZeppelinClient
+from pyzeppelin import ClientConfig, ZeppelinClient, ZSession
 import logging
 
 
@@ -8,8 +8,8 @@ class ZeppelinHook(BaseHook):
     def __init__(self, z_conn):
         self.z_conn = z_conn
         zeppelin_url = "http://" + self.z_conn.host + ":" + str(self.z_conn.port)
-        config = ClientConfig(zeppelin_url)
-        self.z_client = ZeppelinClient(config)
+        self.client_config = ClientConfig(zeppelin_url)
+        self.z_client = ZeppelinClient(self.client_config)
         # if z_conn.login and z_conn.password:
         #    self.z_client.login(z_conn.login, z_conn.password)
 
@@ -25,5 +25,12 @@ class ZeppelinHook(BaseHook):
         else:
             logging.info("note {} is executed successfully".format(note_id))
 
+    def run_code(self, interpreter, code, sub_interpreter = '', intp_properties = {}):
+        z_session = ZSession(self.client_config, interpreter, intp_properties)
+        result = z_session.execute(code, sub_interpreter)
+        if result.is_success:
+            logging.info('Run successfully')
+        else:
+            raise Exception('Fail to run code, execute_result: {}'.format(result))
 
-    
+
--- airflow/contrib/operators/zeppelin_operator.py
+++ airflow/contrib/operators/zeppelin_operator.py
@@ -9,11 +9,24 @@ class ZeppelinOperator(BaseOperator):
     def __init__(self,
                  conn_id,
                  note_id,
+                 interpreter,
+                 intp_properties,
+                 code,
                  *args,
                  **kwargs):
         super(ZeppelinOperator, self).__init__(*args, **kwargs)
         self.note_id = note_id
+        self.interpreter = interpreter
+        self.intp_properties = intp_properties
+        self.code = code
         self.z_hook = ZeppelinHook.get_hook(conn_id)
 
     def execute(self, context):
-        self.z_hook.run_note(self.note_id)
+        if self.note_id:
+            self.z_hook.run_note(self.note_id)
+        else:
+            if not self.interpreter:
+                raise Exception('interpreter is not specified')
+            if not self.code:
+                raise Exception('code is not specified')
+            self.z_hook.run_code(self.interpreter, self.code, self.intp_properties)
